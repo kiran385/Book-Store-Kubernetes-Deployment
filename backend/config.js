68a168867e120d135ec29079c4d25e73f68b4261
kiran385 <145046362+kiran385@users.noreply.github.com>
2024-11-10 10:34:29 +0530

    Add files via upload

---
+++ backend/config.js
@@ -0,0 +1,2 @@
+export const PORT = 5555;
+export const mongoDBURL = 'mongodb+srv://22it060:22it060@bookstore-mern.98umkhj.mongodb.net/books-collection?retryWrites=true&w=majority&appName=BookStore-MERN'
